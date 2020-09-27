ce55daf95d734bdbb31dec3fca525a991de9b4d4
Raman Thakur <me.raman.thakur@gmail.com>
2020-09-27 21:52:08 +0530

    27-09-2020

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/1239422/pexels-photo-1239422.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/266436/pexels-photo-266436.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "raman",
-    image:
-      "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-];
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  discription: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  // res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function (err, allCampgrounds) {
+    if (err) console.log(err);
+    else {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    }
+  });
 });
 app.post("/campgrounds", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newcampground = { name: name, image: image };
-  campgrounds.push(newcampground);
-  res.redirect("/campgrounds");
+  var discription = req.body.discription;
+  var newcampground = { name: name, image: image, discription: discription };
+  Campground.create(newcampground, function (err, newone) {
+    if (err) console.log(err);
+    else {
+      res.redirect("/campgrounds");
+    }
+  });
+  // campgrounds.push(newcampground);
 });
 
 app.get("/campgrounds/new", function (req, res) {
   res.render("new");
 });
-app.listen(3021, function () {
+
+app.get("/campgrounds/:id", function (req, res) {
+  Campground.findById(req.params.id, function (err, foundcampground) {
+    if (err) console.log(err);
+    else {
+      res.render("show", { campground: foundcampground });
+    }
+  });
+});
+app.listen(3022, function () {
   console.log("Example app listening on port 3000!");
 });
